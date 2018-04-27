a527626a7b6ec0b214677f3d03a97a806a042e91
Rodrigo Zea <zearodrigo37@gmail.com>
2018-04-26 21:01:47 -0600

    Commit inicial, ojalá no crashee :(

---
+++ AkariTTR/Assets/Scripts/playerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+    public float speed = 10f, jumpForce = 10f;
+    Rigidbody2D rb;
+    float hInput = 0;
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void move(float horizontalInput)
+    {
+
+    }
+
+    public void startMoving(float horizontalInput){
+        hInput = horizontalInput;
+    }
+
+}
